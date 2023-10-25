7bc56e874ab208fc1c8352507eeb4b640cb03a3f
Nino Keldishvili <Ninokeldishvili@gmail.com>
2023-10-25 13:09:28 +0400

    Add hero and import images

---
+++ src/images/images.js
@@ -0,0 +1,9 @@
+import logo from '../images/interwoven-logo.svg'
+import hero from '../images/interwoven-hero.webp'
+import hero_mobile from '../images/interwoven-mobile.webp'
+
+export const images = {
+    logo,
+    hero,
+    hero_mobile
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+    import "../app.css";
+    import {images} from '../images/images.js'
+</script>
+
+<img src={images.hero} alt="interwoven" class="hero"/>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<style>
+    .hero {
+        width: 100%;
+    }
+</style>
