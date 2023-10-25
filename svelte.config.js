2b5572bd85e05e1c67878de8c513cec24fb3a0af
Nino Keldishvili <Ninokeldishvili@gmail.com>
2023-10-25 13:14:57 +0400

    Update svelte config for build

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,27 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	compilerOptions: {
+		enableSourcemap: true
+	},
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html',
+			precompress: false,
+			strict: true,
+		})
+	},
+	preprocess: vitePreprocess(),
+	vite: {
+		css: {
+			devSourcemap: true
+		}
 	}
 };
-
 export default config;
